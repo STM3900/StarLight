bc936099dc6eb1003609966448477c2646021e81
Théo <theo_38180@live.fr>
2019-08-11 19:08:02 +0200

    Ajout de la page "Selection", elle est responsive

---
+++ musique.js
@@ -0,0 +1,21 @@
+/*const musique = new Audio("medias/musique.mp3");
+musique.currentTime = sessionStorage.getItem('musicTime') || 0;
+musique.volume = 0.1;
+musique.addEventListener('timeupdate', function () {
+    sessionStorage.setItem('musicTime', musique.currentTime);
+  });
+
+  musique.addEventListener('canplay', function () {
+    musique.play().catch(error => console.log(error));
+  })*/
+
+/*var sound = new Howl({
+    src: ['musique.mp3'],
+    autoplay: true,
+    loop: true,
+    volume: 0.5,
+    onend: function() {
+      console.log('Finished!');
+    }
+});*/
+
